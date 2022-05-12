2238fb120479b4a920867896ede15ca99a1d5b9b
Wallace Law <wallace.law@hotmail.com>
2022-05-12 00:05:05 -0400

    Finished game
    Add conditions for winning, losing, and ties
    Add prompt for user input

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const choice = ["Rock", "Paper", "Scissors"]
+let playerChoice;
 
 function computerPlay() {
     return compChoice = choice[Math.floor(Math.random()*choice.length)];
 }
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors" || playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock" || playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return "Tie!";
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playerChoice = prompt("Please select either Rock, Paper, Scissors");
+        console.log(playRound(playerChoice, computerPlay()));
+    }
+}
+
+game();
